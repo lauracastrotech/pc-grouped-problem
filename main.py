a96021319667c7a15ec0c54051f21d7fe513398c
Laura Castro <lauracca@amazon.com>
2025-09-26 16:00:19 -0700

    attempts problem

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
+'''
+what if my list contains duplicates words? assume all unique words
+input: list of strings
+output: set of tuple
+
+retrospective:
+feedback it was good to understand the problem, but with such limited time it's better to code as you go, but you can work through it as, spend less time on pseudo code 
+
+i was proud when i figured out the the value can be a list, and i can later conver it to a tuple and add it to the set 
+'''
 def grouped(s):
+    # initialize set
+    ag_set = set()
+    ag_hash = {}
+
+    # loop through list of words
+    for word in s:
+        # add list comprehension
+        sorted_word = word.split("").sort() # refactor to use list comprehension 
+    # create dictionary where the key is the letters sorted, and the value is a list 
+    # if the key exists, i want to append the word to the list
+
+    # loop through the dictionary values
+    # convert list to a tuple
+    # add tuple to set
+    # return set
     pass
+
+
 
 
 lst = ["eat", "tea", "tan", "ate", "nat", "bat"]
